ac5e1d265baf4bc8f7006f084f7a197c459c2017
1024小神 <15670339118@163.com>
2025-07-11 22:33:40 +0800

    Implement dynamic import for App component based on device type; rename isMobileDevice to isMobile for clarity.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@/assets/index.css'
-import App from './App.tsx'
+import React from 'react'
+import { isMobile } from './utils/common'
+
+const App = React.lazy(() =>
+    isMobile() ? import('./mobile/index') : import('./web/index')
+)
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,4 +1,4 @@
-export const isMobileDevice = () => {
+export const isMobile = () => {
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
         navigator.userAgent
     )
